4113434bedd9b46dddf9dd7a46a93feb219f15c5
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-05-21 16:44:42 -0700

    adds routes

--- server.js
+++ server.js
@@ -6,12 +6,23 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const projects = require("./projects.json");
+const about = require("./about.json");
 
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'))
-app.get("/about", cors(), (req, res) => {
+
+app.get("/", (req, res) => {
+  res.json("Hello World")
+})
+
+app.get("/about", (req, res) => {
   res.json(about);
+})
+
+app.get("/projects", (req, res) => {
+  res.json(projects)
 })
 
 app.listen(PORT, (req, res) => {
